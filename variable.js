054174e3c115a173e2c8a7c6f9bab27def5d53f4
Denzel Turambi <dturambi26@gmail.com>
2023-01-11 00:52:28 -0700

    Add edit

--- variable.js
+++ variable.js
@@ -1,13 +1,24 @@
 
 
 var firstName = "Denzel"
-var firstName = "Timothy"
-var firstName = "Eldridge"
- 
 var age = 22
-var age = 19
-var age = 27
+canDrink = true
 
-canDrink = true
+var firstName = "Timothy"
+var age = 19
 canDrink = false
+
+var firstName = "Eldridge"
+ var age = 27
 canDrink = true
+
+
+
+
+
+
+
+console.log(firstName)
+console.log(age)
+console.log(canDrink)
+
