985bdfef0b768bf4f69db2d4e5254d35658b4a73
Denzel Turambi <dturambi26@gmail.com>
2023-01-11 00:53:54 -0700

    Change order

--- variable.js
+++ variable.js
@@ -3,22 +3,20 @@
 var firstName = "Denzel"
 var age = 22
 canDrink = true
-
-var firstName = "Timothy"
-var age = 19
-canDrink = false
-
-var firstName = "Eldridge"
- var age = 27
-canDrink = true
-
-
-
-
-
-
-
 console.log(firstName)
 console.log(age)
 console.log(canDrink)
 
+var firstName = "Timothy"
+var age = 19
+canDrink = false
+console.log(firstName)
+console.log(age)
+console.log(canDrink)
+
+var firstName = "Eldridge"
+ var age = 27
+canDrink = true
+console.log(firstName)
+console.log(age)
+console.log(canDrink)
