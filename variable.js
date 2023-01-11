cd6ee708fdeaf10e3495d65afb82583f04e2e49a
Denzel Turambi <dturambi26@gmail.com>
2023-01-11 00:16:25 -0700

    Add brothers

---
+++ variable.js
@@ -0,0 +1,28 @@
+"Denzel"
+"Timothy"
+"Eldridge"
+
+22
+19
+27
+
+true
+false
+true
+
+var name
+var age
+var canDrink
+
+var name = "Denzel"
+var name = "Timothy"
+var name = "Eldridge"
+
+var age = 22
+var age = 19
+var age = 27
+
+var canDrink = true
+var canDrink = false
+var canDrink = true
+
