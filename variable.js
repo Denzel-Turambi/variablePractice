4d11ab280692dfa799a09afd6b9f35b5e000479d
Denzel Turambi <dturambi26@gmail.com>
2023-01-11 13:55:37 -0700

    Add consoles

--- variable.js
+++ variable.js
@@ -1,7 +1,7 @@
 
 
 var firstName = "Denzel"
-var birhtday = "0/26/2000"
+var birhtday = "1/26/2000"
 var username = "mokesnipes13"
 
 var age = 22
@@ -12,3 +12,16 @@ var depositPaid = true
 var emptyBalance = false
 var canRent = false
 
+firstName = "Zel"
+birthday = "2/26/2000"
+username = "hamburglar24"
+
+console.log(firstName)
+console.log(birthday)
+console.log(username)
+console.log(age)
+console.log(totalPaid)
+console.log(remainingBalance)
+console.log(depositPaid)
+console.log(emptyBalance)
+console.log(canRent)
