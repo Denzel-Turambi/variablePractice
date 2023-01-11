18c1748bda347a00ec1512560cb6a47cc7d029fa
Denzel Turambi <dturambi26@gmail.com>
2023-01-11 00:46:55 -0700

    Add names

--- variable.js
+++ variable.js
@@ -3,16 +3,4 @@
 var firstName = "Denzel"
 var firstName = "Timothy"
 var firstName = "Eldridge"
-
-var age = 22
-var age = 19
-var age = 27
-
-var canDrink = true
-var canDrink = false
-var canDrink = true
-
-console.log(firstName)
-console.log(age)
-console.log(canDrink)
-
+ 
